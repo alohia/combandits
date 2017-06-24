eb755fe1e787553f881e239727293ce30f12b681
AngusMcKay <angusjamesmckay@gmail.com>
2017-06-24 23:11:05 +0200

    updates

--- adversarial networks UCB1 vs FPL-TRiX.py
+++ adversarial networks UCB1 vs FPL-TRiX.py
@@ -26,6 +26,17 @@ iters = 10000
 # Does UCB1 work more or less the same when subtracting the bound adjustment instead of adding (i.e. more like LCB)?
 # For FPL does adding the random perturbations instead of subtracting them work the same?
 
+################################################
+### impurity function to measure exploration ###
+################################################
+def impurity(path_matrix):
+    row_sums = np.sum(path_matrix, axis=1)
+    pc_chosen = row_sums/np.shape(path_matrix)[1]
+    row_impurities = pc_chosen*(1-pc_chosen)*4
+    average_impurity = np.mean(row_impurities)
+    impurity_score = average_impurity*1000
+    return impurity_score
+
 
 ##################################################################
 ### setup network which changes after a certain length of time ###
@@ -46,7 +57,7 @@ path_means2[0,-1] = 1 # to try to make sure best path isn't to go straight to th
 # trunc norm params
 lower = 0
 upper = 1
-sigma = 0.1
+sigma = 0.01
 
 ### arrays to store results form each iteration
 U = np.zeros((L, iters))
@@ -55,7 +66,6 @@ w = np.zeros((L, iters))
 w_observed = np.zeros((L, iters))
 
 exploration_parameter = 1.5
-
 
 ### INIT
 # draw weights for all iterations
@@ -115,6 +125,11 @@ for t in tqdm(range(1, iters)):
     w_observed[:,t] = w[:,t]*A[:,t]
     c[A[:,t]==1] += 1
 
+# calculature total cost
+adversarial_network_UCB_paths = A[:]
+adversrial_network_UCB_cost = np.sum(adversarial_network_UCB_paths*w)
+adversrial_network_UCB_exploration_score = impurity(adversarial_network_UCB_paths)
+
 
 
 ### find shortest paths for each iteration
@@ -151,15 +166,14 @@ for t in tqdm(range(iters)):
     adversarial_network_optimal_paths[:,t] = np.concatenate(shortest_path_matrix)
 
 # calculate total costs
-adversrial_network_UCB_cost = np.sum(A*w)
 adversrial_network_optimal_path_cost = np.sum(adversarial_network_optimal_paths*w)
 
 
 
 ### FPL-TRiX on same adversarial network
 # model params
-eta = 1 # this acts against the perturbations, so the higher this is the less significant the perturbations in the combinatorial optimisation
-gamma = 0.1 # the higher this is the more the alorithm explores
+eta = 2 # this acts against the perturbations, so the higher this is the less significant the perturbations in the combinatorial optimisation
+gamma = 0.0001 # the lower this is the more the alorithm explores
 Bt = 0.5 # controls the perturbations, increasing this should increase exploration
 
 # matrices to store results
@@ -212,7 +226,29 @@ for t in tqdm(range(1, iters)):
     # update L_hats
     L_hat[:,t] = L_hat[:,(t-1)] + l_hat[:,t]
 
-adversrial_network_FPL_cost = np.sum(A*w)
+adversarial_network_FPL_paths = A[:]
+adversrial_network_FPL_cost = np.sum(adversarial_network_FPL_paths*w)
+adversrial_network_FPL_exploration_score = impurity(adversarial_network_FPL_paths)
+
+plt.pcolormesh(adversarial_network_optimal_paths)
+plt.pcolormesh(adversarial_network_FPL_paths)
+plt.pcolormesh(adversarial_network_UCB_paths)
+
+
+# first 1000 iters
+np.sum(adversarial_network_optimal_paths[:,:1000]*w[:,:1000])
+np.sum(adversarial_network_UCB_paths[:,:1000]*w[:,:1000])
+np.sum(adversarial_network_FPL_paths[:,:1000]*w[:,:1000])
+# middle 1000
+np.sum(adversarial_network_optimal_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(adversarial_network_UCB_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(adversarial_network_FPL_paths[:,4500:5500]*w[:,4500:5500])
+# last 1000
+np.sum(adversarial_network_optimal_paths[:,9000:]*w[:,9000:])
+np.sum(adversarial_network_UCB_paths[:,9000:]*w[:,9000:])
+np.sum(adversarial_network_FPL_paths[:,9000:]*w[:,9000:])
+
+
 
 
 
@@ -224,6 +260,11 @@ adversrial_network_FPL_cost = np.sum(A*w)
 diag_zero = np.ones(L).reshape(V, V) - np.diag(np.ones(V))
 
 path_means3 = np.ones(L).reshape(V, V)
+for i in range(V):
+    for j in range(V):
+        path_means3[i,j] = abs(i - j)/V + 5/(V*2)
+path_means3 = abs(path_means3 * diag_zero)
+
 
 path_means4 = np.ones(L).reshape(V, V)
 for i in range(V-1):
@@ -235,7 +276,7 @@ for i in range(V-1):
 # trunc norm params
 lower = 0
 upper = 1
-sigma = 0.1
+sigma = 0.01
 
 ### arrays to store results form each iteration
 U = np.zeros((L, iters))
@@ -251,7 +292,7 @@ exploration_parameter = 1.5
 # Draw new weights and store in w
 for t in range(iters):
     # select path means
-    if t < iters/2:
+    if int(t/1000)/2==int(int(t/1000)/2):
         path_means = path_means3
     else:
         path_means = path_means4
@@ -304,6 +345,10 @@ for t in tqdm(range(1, iters)):
     w_observed[:,t] = w[:,t]*A[:,t]
     c[A[:,t]==1] += 1
 
+# calculate total costs
+adversarial_network2_UCB_paths = A[:]
+adversrial_network2_UCB_cost = np.sum(adversarial_network2_UCB_paths*w)
+adversrial_network2_UCB_exploration_score = impurity(adversarial_network2_UCB_paths)
 
 
 ### find shortest paths for each iteration
@@ -314,7 +359,7 @@ actual_path_means4 = truncnorm.stats(np.concatenate(lower-path_means4)/sigma, np
 adversarial_network2_optimal_paths = np.zeros((L, iters))
 
 for t in tqdm(range(iters)):
-    if t < iters/2:
+    if int(t/1000)/2==int(int(t/1000)/2):
         path_means = path_means3
     else:
         path_means = path_means4
@@ -340,15 +385,13 @@ for t in tqdm(range(iters)):
         shortest_path_matrix[shortest_path[i], shortest_path[i+1]] = 1
     adversarial_network2_optimal_paths[:,t] = np.concatenate(shortest_path_matrix)
 
-# calculate total costs
-adversrial_network2_UCB_cost = np.sum(A*w)
 adversrial_network2_optimal_path_cost = np.sum(adversarial_network2_optimal_paths*w)
 
 
 ### FPL-TRiX on same adversarial network
 # model params
-eta = 1 # this acts against the perturbations, so the higher this is the less significant the perturbations in the combinatorial optimisation
-gamma = 0.1 # the higher this is the more the alorithm explores
+eta = 0.1 # this acts against the perturbations, so the higher this is the less significant the perturbations in the combinatorial optimisation
+gamma = 0.001 # the higher this is the more the alorithm explores
 Bt = 0.5 # controls the perturbations, increasing this should increase exploration
 
 # matrices to store results
@@ -401,14 +444,59 @@ for t in tqdm(range(1, iters)):
     # update L_hats
     L_hat[:,t] = L_hat[:,(t-1)] + l_hat[:,t]
 
-adversrial_network2_FPL_cost = np.sum(A*w)
+adversarial_network2_FPL_paths = A[:]
+adversrial_network2_FPL_cost = np.sum(adversarial_network2_FPL_paths*w)
+adversrial_network2_FPL_exploration_score = impurity(adversarial_network2_FPL_paths)
+
+plt.pcolormesh(adversarial_network2_optimal_paths)
+plt.pcolormesh(adversarial_network2_UCB_paths[:,:])
+plt.pcolormesh(adversarial_network2_FPL_paths[:,:])
+
+
+# first 1000 iters
+np.sum(adversarial_network2_optimal_paths[:,:1000]*w[:,:1000])
+np.sum(adversarial_network2_UCB_paths[:,:1000]*w[:,:1000])
+np.sum(adversarial_network2_FPL_paths[:,:1000]*w[:,:1000])
+# middle 1000
+np.sum(adversarial_network2_optimal_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(adversarial_network2_UCB_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(adversarial_network2_FPL_paths[:,4500:5500]*w[:,4500:5500])
+# last 1000
+np.sum(adversarial_network2_optimal_paths[:,9000:]*w[:,9000:])
+np.sum(adversarial_network2_UCB_paths[:,9000:]*w[:,9000:])
+np.sum(adversarial_network2_FPL_paths[:,9000:]*w[:,9000:])
+
+# last 1000
+np.sum(adversarial_network2_optimal_paths[:,8000:9000]*w[:,8000:9000])
+np.sum(adversarial_network2_UCB_paths[:,8000:9000]*w[:,8000:9000])
+np.sum(adversarial_network2_FPL_paths[:,8000:9000]*w[:,8000:9000])
+
+
+
+### plot increase in cost over time
+adversarial_network2_UCB_cumul_cost = np.zeros(iters/10)
+adversarial_network2_FPL_cumul_cost = np.zeros(iters/10)
+for t in tqdm(range(0, iters, 10)):
+    adversarial_network2_UCB_cumul_cost[t/10] = np.sum(adversarial_network2_UCB_paths[:,:t]*w[:,:t])
+    adversarial_network2_FPL_cumul_cost[t/10] = np.sum(adversarial_network2_FPL_paths[:,:t]*w[:,:t])
+
+x_axis = list(range(0, iters, 10))
+
+plt.plot(x_axis, adversarial_network2_UCB_cumul_cost, linewidth = 2.5, c = (1, 0.5, 0.0), alpha = 1, label = "CombUCB1")
+plt.plot(x_axis, adversarial_network2_FPL_cumul_cost, linewidth = 2.5, c = (0.0, 0.5, 1), alpha = 1, label = "FPL-TRiX")
+plt.xlabel("iteration")
+plt.ylabel("cumulative cost")
+plt.title("Adversarial network 2 cumulative cost")
+plt.legend(ncol = 2)
+plt.savefig('advers_net2_cumul_costs.jpg')
+
 
 
 
 ###############################################################
 ### setup network that changes each iteration with sin wave ###
 ###############################################################
-iters = 100000
+iters = 10000
 # setup different 'shifts' to the sine wave so that different paths have different starting points on the wave
 sine_wave_shift = np.zeros(L).reshape(V, V)
 for i in range(V):
@@ -416,7 +504,7 @@ for i in range(V):
         sine_wave_shift[i,j] = abs(i - j)/(V)
 
 # parameter which determines how many repetitions of the sine wave
-sine_wave_reps = 0.5
+sine_wave_reps = 2
 
 # set an adjustment to subtract from each path weight mean to encourage longer paths
 adjust = 0.00
@@ -424,12 +512,16 @@ adjust = 0.00
 # setting trunc norm means for each iteration
 trunc_norm_means = np.zeros((L, iters))
 for t in range(iters):
-    trunc_norm_means[:,t] = np.concatenate(abs(np.sin((sine_wave_shift+t*sine_wave_reps/(iters))*np.pi)-adjust)*diag_zero)
+    trunc_norm_means[:,t] = np.concatenate(((np.sin((sine_wave_shift+t*sine_wave_reps/(iters))*np.pi)+1)/2-adjust)*diag_zero)
 
+# draw weights for entire period
 # trunc norm params
 lower = 0
 upper = 1
 sigma = 0.1
+
+w = truncnorm.rvs((lower-trunc_norm_means)/sigma, (upper-trunc_norm_means)/sigma, loc=trunc_norm_means, scale=sigma)
+
 
 ### arrays to store results form each iteration
 U = np.zeros((L, iters))
@@ -440,8 +532,6 @@ exploration_parameter = 1.5
 
 
 ### INIT
-# draw weights for entire period
-w = truncnorm.rvs((lower-trunc_norm_means)/sigma, (upper-trunc_norm_means)/sigma, loc=trunc_norm_means, scale=sigma)
 
 t = 0
 # Initial draw of weight for each element
@@ -487,6 +577,10 @@ for t in tqdm(range(1, iters)):
     w_observed[:,t] = w[:,t]*A[:,t]
     c[A[:,t]==1] += 1
 
+# calculate total costs
+sine_adversrial_network_UCB_paths = A[:]
+sine_adversrial_network_UCB_cost = np.sum(sine_adversrial_network_UCB_paths*w)
+sine_adversrial_network_UCB_exploration_score = impurity(sine_adversrial_network_UCB_paths)
 
 
 ### find shortest paths for each iteration
@@ -520,8 +614,7 @@ for t in tqdm(range(iters)):
     sine_adversary_optimal_paths[:,t] = np.concatenate(shortest_path_matrix)
 
 
-# calculate total costs
-sine_adversrial_network_UCB_cost = np.sum(A*w)
+
 sine_adversrial_network_optimal_path_cost = np.sum(sine_adversary_optimal_paths*w)
 
 sine_UCB_end_cost = np.sum((A*w)[:,70000:])
@@ -583,7 +676,31 @@ for t in tqdm(range(1, iters)):
     # update L_hats
     L_hat[:,t] = L_hat[:,(t-1)] + l_hat[:,t]
 
-sine_adversrial_network_FPL_cost = np.sum(A*w)
+sine_adversrial_network_FPL_paths = A[:]
+sine_adversrial_network_FPL_cost = np.sum(sine_adversrial_network_FPL_paths*w)
+sine_adversrial_network_FPL_exploration_score = impurity(sine_adversrial_network_FPL_paths)
 
-sine_FPL_end_cost = np.sum((A*w)[:,70000:])
+
+plt.pcolormesh(sine_adversrial_network_FPL_paths[:,:10000])
+plt.pcolormesh(sine_adversrial_network_UCB_paths[:,:10000])
+plt.pcolormesh(sine_adversary_optimal_paths[:,:10000])
+
+# first 1000 iters
+np.sum(sine_adversary_optimal_paths[:,:1000]*w[:,:1000])
+np.sum(sine_adversrial_network_UCB_paths[:,:1000]*w[:,:1000])
+np.sum(sine_adversrial_network_FPL_paths[:,:1000]*w[:,:1000])
+# middle 1000
+np.sum(sine_adversary_optimal_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(sine_adversrial_network_UCB_paths[:,4500:5500]*w[:,4500:5500])
+np.sum(sine_adversrial_network_FPL_paths[:,4500:5500]*w[:,4500:5500])
+# last 1000
+np.sum(sine_adversary_optimal_paths[:,9000:]*w[:,9000:])
+np.sum(sine_adversrial_network_UCB_paths[:,9000:]*w[:,9000:])
+np.sum(sine_adversrial_network_FPL_paths[:,9000:]*w[:,9000:])
+
+
+
+### LOOK AT EXPLORATION OVER TIME
+
+
 
